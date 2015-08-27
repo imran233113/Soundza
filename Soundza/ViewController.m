e4ba244a2c35cabf84660b2d3d3240f8ab129437
afriedman94 <andrew.friedman94@gmail.com>
2015-08-27 14:11:20 -0400

    Initial Commit

---
+++ Soundza/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Soundza
+//
+//  Created by Andrew Friedman on 8/27/15.
+//  Copyright (c) 2015 Andrew Friedman. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
